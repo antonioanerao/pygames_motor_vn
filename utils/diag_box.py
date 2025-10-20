ee94aa4109c6217029cadc5b3ba50d4fbbc354fe
Antonio Anerão <anerao.junior@gmail.com>
2025-10-20 10:49:23 -0500

    update: ajuste na transparencia da caixa de dialogo; Caixa de dialogo por padrao agora recebe a altura da tela * 0.15%

--- utils/diag_box.py
+++ utils/diag_box.py
@@ -3,7 +3,12 @@ import settings
 import colors
 
 
-def draw_dialog_box(display_surface, rect_offset_y, rect_width=settings.WIDTH, rect_height=220, padding=5):
+def draw_dialog_box(
+        display_surface,
+        rect_offset_y,
+        rect_width=settings.WIDTH,
+        rect_height=settings.HEIGHT * 0.15,
+        padding=5):
     rect = pygame.Rect(0, rect_offset_y, rect_width, rect_height)
 
     box_surface = pygame.Surface((rect_width, rect_height), pygame.SRCALPHA)
@@ -14,7 +19,7 @@ def draw_dialog_box(display_surface, rect_offset_y, rect_width=settings.WIDTH, r
         rect_height - padding * 2
     )
 
-    pygame.draw.rect(box_surface, (255, 255, 255, 150), inner_rect, border_radius=16)
+    pygame.draw.rect(box_surface, (255, 255, 255, 200), inner_rect, border_radius=16)
     pygame.draw.rect(box_surface, colors.SERINGALLAB_DARK, inner_rect, 3, border_radius=16)
 
     display_surface.blit(box_surface, rect)
